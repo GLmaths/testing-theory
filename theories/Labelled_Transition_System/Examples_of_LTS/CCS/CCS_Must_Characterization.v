dda5b643a47716a578a886016269752067e2a34d
GLmaths <gaetanlopez.maths@gmail.com>
2026-04-25 03:51:11 +0200

    CCS alt characterization

--- theories/Labelled_Transition_System/Examples_of_LTS/CCS/CCS_Must_Characterization.v
+++ theories/Labelled_Transition_System/Examples_of_LTS/CCS/CCS_Must_Characterization.v
@@ -29,23 +29,14 @@ From Stdlib.Strings Require Import String.
 From stdpp Require Import base countable finite gmap list gmultiset strings.
 From Stdlib Require Import Relations.
 From Stdlib.Wellfounded Require Import Inverse_Image.
-From TestingTheory Require Import CCSInstance DefinitionAS Must Testing_Predicate ParallelLTSConstruction ActTau InputOutputActions.
+From TestingTheory Require Import VCCS_Must_Characterization.
 
-Parameter Corollary bhv_iff_ctx_CCS : True.
+Module Type CCS_Must_Alt_Corollary.
+Include VCCS_Must_Alt_Corollary.
 
-(* Corollary bhv_iff_ctx_CCS (p q : proc) : p ⊑ₘᵤₛₜᵢ q <-> p ≼ₐₛ q.
+Axiom Value_is_unit : Value = unit.
+
+Corollary bhv_iff_ctx_CCS (p q : proc) : p ⊑ₘᵤₛₜᵢ q <-> p ≼ₐₛ q.
 Proof.
-  split.
-  - intro Hyp. eapply @equivalence_acc_set_and_must_i; eauto.
-
-    exact ACCS_gLtsFiniteImage. exact ACCS_gLtsFiniteImage. exact ACCS_gLtsOBAFB. exact ACCS_gLtsFiniteImage.
-    exact Interaction_between_FW_ACCS_and_ACCS. exact Interaction_between_FW_ACCS_and_ACCS.
-    exact (@gAbsAction (ExtAct name)). exact gen_conv_gen_spec_conv_inst . exact gen_acc_gen_spec_acc_inst.
-
-  - intro Hyp. eapply @equivalence_acc_set_and_must_i in Hyp; eauto.
-
-    exact ACCS_gLtsFiniteImage. exact ACCS_gLtsFiniteImage. exact ACCS_gLtsOBAFB. exact ACCS_gLtsFiniteImage.
-    exact Interaction_between_FW_ACCS_and_ACCS. exact Interaction_between_FW_ACCS_and_ACCS.
-    exact (@gAbsAction (ExtAct name)). exact gen_conv_gen_spec_conv_inst . exact gen_acc_gen_spec_acc_inst.
-Qed. *)
-
+  (* eapply bhv_iff_ctx_VCCS. *) admit.
+Admitted.
